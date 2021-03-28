00cd8fe33edfaf4c01e74b0997e0ffcb7bb860ef
Ritz GL <maurigarzonlara@hotmail.com>
2021-03-28 13:29:23 +0800

    describe logic in comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // logic for the quiz
+// from the click button
+//  start the timer
+//  question is shown
+//  answer is given
+//      question is either wrong or right
+                // if wrong, timer down
+                // score goes down
+            // display next question
+                // if right score go up
+            // display next question
