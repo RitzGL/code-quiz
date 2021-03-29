3aff38f29356b04e841c4c69f084334394817bac
Ritz GL <maurigarzonlara@hotmail.com>
2021-03-29 14:10:34 +0800

    html edit + object array declaration

--- js/script.js
+++ js/script.js
@@ -9,3 +9,43 @@
             // display next question
                 // if right score go up
             // display next question
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include: ",
+        responses: ["Strings","Numbers","Booleans","Alerts"],
+        answer: "Alerts"
+    },
+    {
+        question: "An Array's individual element can be accessed via its: ",
+        responses: ["Method","Index","Memory","Address"],
+        answer: "Index"
+    },
+    {
+        question: "If '8' is a string, then 8 is a/an: ",
+        responses: ["Number","Boolean","Character","Object"],
+        answer: "Number"
+    },
+    {
+        question: "You can iterate through an Array with a/an: ",
+        responses: ["Do-While-Loop","While-Loop","Iterator","For-Loop"],
+        answer: "For-Loop"
+    },
+    {
+        question: "If I have an Array with 5 elements, what will array.length return? ",
+        responses: ["1","0","4","5"],
+        answer: "5"
+    }
+]
+
+for(let i = 0; i < questions.length; i++){
+
+    console.log("Object" , i, "Question: ", questions[i].question);
+    
+    for(let i = 0; i < questions[i].responses.length; i++){
+        console.log("Response" , i,questions[i].responses[i]);
+    }
+    
+    console.log("Object" , i, "Answer: ", questions[i].answer);
+    
+}
